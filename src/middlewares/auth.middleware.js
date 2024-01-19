efdd880ebf0d06fe840a0be6e3c44b3b7df8b7f3
KwanSiong007 <ksong329@gmail.com>
2024-01-19 23:13:30 +0800

    chore: remove console.log

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,9 +1,6 @@
 const { auth } = require("express-oauth2-jwt-bearer");
 require("dotenv").config();
 
-console.log("Auth0 Audience:", process.env.AUTH0_AUDIENCE);
-console.log("Auth0 Issuer Base URL:", process.env.AUTH0_ISSUER_BASE_URL);
-
 const authMiddleware = auth({
   audience: process.env.AUTH0_AUDIENCE,
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
